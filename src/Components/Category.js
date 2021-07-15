c269d3004989a8169e6fdce3d88e3903a80e4882
Delice Lydia <Delydia84@gmail.com>
2021-07-15 10:24:23 +0200

    Add category component

--- src/Components/Category.js
+++ src/Components/Category.js
@@ -1,1 +1,12 @@
 import PropTypes from 'prop-types';
+import Image from './FoodImg';
+
+const Category = ({ src, name }) => (
+  <Image className="category" src={src} name={name} />
+);
+
+Category.propTypes = {
+  src: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+};
+export default Category;
