9cdaeb4dee28d7812f075cc15a13519b98d44cd7
Delice Lydia <Delydia84@gmail.com>
2021-07-22 18:13:45 +0200

    Add food image component tests

---
+++ src/_Tests_/Components/FoodImg.test.js
@@ -0,0 +1,24 @@
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-17-updated';
+import Image from '../../Components/FoodImg';
+
+Enzyme.configure({ adapter: new Adapter() });
+const setup = () => {
+  const component = shallow(<Image src="jest" name="testing" />);
+  return component;
+};
+
+describe('Header component', () => {
+  let component;
+  beforeEach(() => {
+    component = setup();
+  });
+
+  it('should display one image', () => {
+    expect(component.find('img').hasClass('recipe-image')).toBe(false);
+  });
+
+  it('should display one header', () => {
+    expect(component.find('h2').length).toBe(1);
+  });
+});
