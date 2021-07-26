41fede90ca1b54505180af727096bd0964f80246
Delice Lydia <Delydia84@gmail.com>
2021-07-26 10:50:19 +0200

    Fix deploy error

--- src/Actions/AllCategories.js
+++ src/Actions/AllCategories.js
@@ -1,6 +1,6 @@
 import { fetchCategories, fetchFoodError } from './index';
 
-const fetchAllCategories = () => (dispatch) => {
+const getCategories = () => (dispatch) => {
   fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
     .then((res) => res.json())
     .then((res) => {
@@ -14,4 +14,4 @@ const fetchAllCategories = () => (dispatch) => {
     });
 };
 
-export default fetchAllCategories;
+export default getCategories;
